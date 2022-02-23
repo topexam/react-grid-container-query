e2d0230084666881ae4eab411ff196c1a69f98e8
An Nguyen Q <ngquangan.dev@gmail.com>
2022-02-23 22:57:40 +0700

    test: force test suite

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom';
 
 describe('Thing', () => {
   it('renders without crashing', () => {
-    
+    expect(1 + 1).toBe(2);
   });
 });
